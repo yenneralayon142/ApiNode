c07d484a550f83687d1607068d49864f818467f1
Yenner Alayon <yenneralayon@gmail.com>
2025-09-27 19:11:50 -0500

    feat(backend):Config of project

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,7 @@ const dbConfig = {
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'db_node',
-  port: Number(process.env.DB_PORT) || 3307,
+  port: Number(process.env.DB_PORT) || 3306,
   connectionLimit: Number(process.env.DB_POOL_LIMIT) || 10
 };
 
